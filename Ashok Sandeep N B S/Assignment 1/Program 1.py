87b22f2b2fec205c463fdcf6f46b6869c4903e28
ashoksandeepnbs1987 <113974542+ashoksandeepnbs1987@users.noreply.github.com>
2022-09-20 13:47:42 +0530

    Update Program 1.py

--- Ashok Sandeep N B S/Assignment 1/Program 1.py
+++ Ashok Sandeep N B S/Assignment 1/Program 1.py
@@ -1,1 +1,8 @@
- 
+num = int(input("Enter a number: "))
+if num > 1:
+    for i in range(2, num):
+        if (num % i) == 0:
+            print(num, "is not a prime number")
+            break
+       elif(i==num-1):
+            print(num, "is a prime number")
