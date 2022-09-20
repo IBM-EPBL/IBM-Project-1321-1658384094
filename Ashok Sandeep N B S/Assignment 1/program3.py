b086fda907c6a7309b52cebc26790839fc5f3a7a
ashoksandeepnbs1987 <113974542+ashoksandeepnbs1987@users.noreply.github.com>
2022-09-20 13:50:55 +0530

    Create program3.py

---
+++ Ashok Sandeep N B S/Assignment 1/program3.py
@@ -0,0 +1,8 @@
+upper=int(input("enter the upperlimit:")
+for num in range(upper + 1):
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
