ec4e71afbe7da288ff4eba4dbe483e08f5950691
SANTHOSH V <113985117+santhoshv30@users.noreply.github.com>
2022-09-21 15:47:18 +0530

    Update Program 1.py

--- Santhosh V/Assignment 1/Program 1.py
+++ Santhosh V/Assignment 1/Program 1.py
@@ -1,1 +1,11 @@
- 
+n=int((input()))
+flag = False
+if n > 1:
+    for i in range(2,n):
+        if (n% i) == 0:
+            flag = True
+            break
+if flag:
+    print(n, "is not a prime number")
+else:
+    print(n, "is a prime number")
