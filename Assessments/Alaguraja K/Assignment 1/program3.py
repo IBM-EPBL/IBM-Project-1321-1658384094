69d87aaa143edda0d6e613da77280b7a43fb834b
ibm-nalaiyathiran <113974711+ibm-nalaiyathiran@users.noreply.github.com>
2022-09-27 14:24:04 +0530

    Create program3.py

---
+++ Assessments/Alaguraja K/Assignment 1/program3.py
@@ -0,0 +1,8 @@
+upper = int(input("Enter the upper value:"))
+for number in range(2,upper+1):
+    if number>1:
+        for i in range(2,number):
+            if (number%i)==0:
+                break
+        else:
+            print(number)
